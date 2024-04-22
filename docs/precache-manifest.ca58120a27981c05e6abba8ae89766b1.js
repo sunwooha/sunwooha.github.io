ef74f328150ddbcbc494abf0fd0063c136328e22
sunwooha <sha@wustl.edu>
2024-04-22 13:42:35 -0500

    updated build

---
+++ docs/precache-manifest.ca58120a27981c05e6abba8ae89766b1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "7fda3001da095ecce699",
+    "url": "/static/css/main.de6af84d.chunk.css"
+  },
+  {
+    "revision": "7fda3001da095ecce699",
+    "url": "/static/js/main.7fda3001.chunk.js"
+  },
+  {
+    "revision": "70088631f9ff653a1565",
+    "url": "/static/css/1.e73b1e1a.chunk.css"
+  },
+  {
+    "revision": "70088631f9ff653a1565",
+    "url": "/static/js/1.70088631.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "44e6cf2e0133e9fa84ca2d56d0769146",
+    "url": "/static/media/img.44e6cf2e.jpg"
+  },
+  {
+    "revision": "dcb249ca7608789aaedab1eee0dd1f55",
+    "url": "/static/media/eurovis2024.dcb249ca.pdf"
+  },
+  {
+    "revision": "fd38cc2003152daacdc665f93c4ab0af",
+    "url": "/static/media/cv.fd38cc20.pdf"
+  },
+  {
+    "revision": "d24f9f7da64d37268691f866d40d259b",
+    "url": "/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d5810db29ba5db065944a69fcc14a2fc.js"
+  "/precache-manifest.ca58120a27981c05e6abba8ae89766b1.js"
 );
 
 workbox.clientsClaim();
